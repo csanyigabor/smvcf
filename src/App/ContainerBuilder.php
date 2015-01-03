02cff05295971b5ed748f5e660c3b3ea64c5de9e
Gabor Csanyi <csanyi.gabor@webndev.hu>
2015-01-03 21:03:28 +0100

    fixing form factory service

--- src/App/ContainerBuilder.php
+++ src/App/ContainerBuilder.php
@@ -9,6 +9,7 @@ use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
 use Symfony\Component\Form\Extension\Csrf\CsrfProvider\SessionCsrfProvider;
 use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
+use Symfony\Component\Form\Forms;
 use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Templating\Helper\SlotsHelper;
@@ -113,13 +114,21 @@ class ContainerBuilder
 
         $httpExtension = new HttpFoundationExtension();
 
+        $formFactoryBuilder = Forms::createFormFactoryBuilder()
+            ->addExtension($csrfExtension)
+            ->addExtension($httpExtension);
+
         $container
             ->register(
                 'form_factory',
-                'Symfony\Component\HttpFoundation\Session\Session'
+                'Symfony\Component\Form\FormFactory'
             )
-            -addMethodCall('addExtension', $csrfExtension)
-            -addMethodCall('addExtension', $httpExtension)
+            ->setFactory(
+                [
+                    $formFactoryBuilder,
+                    'getFormFactory'
+                ]
+            )
         ;
     }
 }
